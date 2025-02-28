799fdf5f259cf407772906e2633bbae16b1183d6
ApollineSetan <setan.apolline@gmail.com>
2025-02-28 13:54:33 +0100

    equilibrage stats persos

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import com.apolline.tourpartour.combat.Partie;
 public class Main {
     public static void main(String[] args){
         // Création des personnages
-        Assassin assassin = new Assassin("Ezio", 20,10,5,3);
-        Guerrier guerrier = new Guerrier("Harald", 60, 2, 7, 4);
+        Assassin assassin = new Assassin("Ezio", 30,12,4,3);
+        Guerrier guerrier = new Guerrier("Harald", 50, 8, 6, 3);
 
         // Création de la partie
         Partie partieTest = new Partie(assassin, guerrier, 10);
